97e35d180b87cc975fee95b0cf4c8c40b6003b32
Diablohu <diablohudream@gmail.com>
2017-07-28 15:32:20 +0800

    fix

--- onServerRender/index.js
+++ onServerRender/index.js
@@ -1,36 +1,11 @@
-import cookie from 'cookie'
-import { CHANGE_LANGUAGE } from './redux'
 import {
     actionInit,
     actionLocales
-} from '../'
+} from 'sp-i18n'
 
 export default (obj) => {
-    let { koaCtx, reduxStore } = obj
-
-    let lang = (() => {
-
-        // 先查看URL参数是否有语音设置
-        // hl 这个参数名是参考了Instargram
-        let lang = koaCtx.query.hl
-
-        // 如果没有，检查cookie
-        const cookies = cookie.parse(koaCtx.request.header.cookie || '')
-        if (!lang && cookies.spLocaleId && cookies.spLocaleId !== 'null')
-            lang = cookies.spLocaleId
-
-        // 如果没有，再看header里是否有语言设置
-        if (!lang)
-            lang = koaCtx.header['accept-language']
-
-        // 如没有，再用默认
-        if (!lang)
-            lang = 'en'
-
-        return lang
-    })()
-
-    reduxStore.dispatch({ type: CHANGE_LANGUAGE, data: lang })
+    let { reduxStore } = obj
+    
     reduxStore.dispatch(actionInit(reduxStore.getState()))
     reduxStore.dispatch(actionLocales())
 
